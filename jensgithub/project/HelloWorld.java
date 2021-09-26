59674d1070663243adbea8e53ff12f4eb76c3023
pnajahhidayani <pnajahhidayanidex@gmail.com>
2021-09-26 21:37:27 +0700

    HelloWorld.java

---
+++ jensgithub/project/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+public static void main (String []args){
+System.out.println ("Nama: Putri Najah Hidayani");
+System.out.println ("NIM: 6706213029");
+System.out.println ("Kelas: 45-05");
+
+}
+}
